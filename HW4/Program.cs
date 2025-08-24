7d96a59e9d05f2db56bdddf1c84bdfbc1811526f
ttuniversal <tsitelishvilitekla123@gmail.com>
2025-08-25 02:03:37 +0400

    Updated HW 4

---
+++ HW4/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Hello, please enter your name: ");
+String input = Console.ReadLine();
+Console.WriteLine("Hello, " + input + " Welcome to my fisrt console app");
