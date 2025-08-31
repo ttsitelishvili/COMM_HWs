0a37c2cd97abe8cf5628de163616d337ac67fcef
ttuniversal <tsitelishvilitekla123@gmail.com>
2025-08-31 22:20:37 +0400

    correcting mistakes

--- HW4/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿
-Console.BackgroundColor = ConsoleColor.DarkBlue;
-Console.ForegroundColor = ConsoleColor.White;
-
-Console.WriteLine("Hello, please enter your name: ");
-String input = Console.ReadLine();
-Console.WriteLine("Hello, " + input + " Welcome to my fisrt console app");
